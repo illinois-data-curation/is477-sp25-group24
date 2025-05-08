9fd3590d3ab685e42fc6cea8a5d4159b54dafd4a
Alejandra COrnejo <ac122@illinois.edu>
2025-05-08 17:22:53 -0500

    FinalProject

--- FInalProject/fix_json.py
+++ FInalProject/fix_json.py
@@ -8,4 +8,4 @@ parsed = [json.loads(line) for line in lines]
 with open("data/world_bank_data_fixed.json", "w") as f:
     json.dump(parsed, f, indent=2)
 
-print("✅ Converted raw JSON to valid JSON array.")
+print(" Converted raw JSON to valid JSON array.")
